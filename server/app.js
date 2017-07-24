23ddb21ef11597a1796e2022096cfb22dafbdb18
Vova Hooke <vladimir.hooke@gmail.com>
2017-07-24 17:01:50 +0300

    added console log url

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
 const PORT = 3000;
 const IP = "http://localhost";
 
 app.get('/', (req, res) => {
+  console.log(req.url)
   res.send('Hello World!')
 })
 
