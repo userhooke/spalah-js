5b1413e6410c0c08e0c849bf4b55ddd7f9faf6c1
Vladimir Hooke <vpelet@gmail.com>
2017-07-24 14:06:00 +0000

    conflict resolved

--- server/app.js
+++ server/app.js
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT;
-const IP = process.env.IP;
-=======
 const express = require('express');
 const app = express();
 const fs = require('fs');
 
-const PORT = 3000;
-const IP = "http://localhost";
->>>>>>> 23ddb21ef11597a1796e2022096cfb22dafbdb18
+const PORT = process.env.PORT;
+const IP = process.env.IP;
 
 app.get('/', (req, res) => {
   console.log(req.url)
